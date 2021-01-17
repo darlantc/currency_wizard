07e47e87eb6ba4f4ce1d7e6d59716f43ee1e019c
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-01-16 23:53:41 -0300

    test: proved that CurrencyServiceStub implements requestCurrencyOptions

--- CurrencyWizardTests/CurrencyServiceTests.swift
+++ CurrencyWizardTests/CurrencyServiceTests.swift
@@ -12,24 +12,57 @@ import XCTest
 struct CurrencyOption {
 	let name: String
 	let id: String
-	let isFavorite: Bool
 }
 
 protocol CurrencyService {
-	func requestCurrencyOptions(completion: ([CurrencyOption]))
+	func requestCurrencyOptions(completion: ([CurrencyOption]) -> Void)
 }
 
 class CurrencyServiceTests: XCTestCase {
 	func test_initSUT_shouldNotRequestAnything() {
-		let sut = CurrencyServiceStub()
-		
+		let sut = makeSUT()
 		XCTAssertEqual(sut.internalCalls, [])
+	}
+	
+	func test_requestCurrencyOptions_shouldRequestCurrencyOptions() {
+		var requestCount = 0
+		
+		func requestCallback(options: [CurrencyOption]) -> Void {
+			requestCount += 1
+		}
+		
+		let sut = makeSUT()
+		sut.requestCurrencyOptions(completion: requestCallback)
+		XCTAssertEqual(requestCount, 1)
+		XCTAssertEqual(sut.internalCalls, ["requestCurrencyOptions"])
+		
+		sut.requestCurrencyOptions(completion: requestCallback)
+		XCTAssertEqual(requestCount, 2)
+		XCTAssertEqual(sut.internalCalls, ["requestCurrencyOptions", "requestCurrencyOptions"])
+	}
+	
+	func test_requestCurrencyOptions_shouldReturnValidData() {
+		var response = [CurrencyOption]()
+		
+		let sut = makeSUT()
+		sut.requestCurrencyOptions { response = $0 }
+
+		XCTAssertFalse(response.isEmpty)
+	}
+	
+	
+	// MARK: Helpers
+	private func makeSUT() -> CurrencyServiceStub {
+		return CurrencyServiceStub()
 	}
 }
 
 private class CurrencyServiceStub: CurrencyService {
-	func requestCurrencyOptions(completion: ([CurrencyOption])) {
-		
+	func requestCurrencyOptions(completion: ([CurrencyOption]) -> Void) {
+		self.didCall(function: "requestCurrencyOptions")
+		completion([
+			CurrencyOption(name: "United States Dollar", id: "USD")
+		])
 	}
 	
 	var internalCalls = [String]()
