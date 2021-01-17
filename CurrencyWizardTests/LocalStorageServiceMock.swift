2f1f6f80fff7b2df8d97150659a0de11552bbfab
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-01-17 10:58:19 -0300

    test: added helper method didCallListener to LocalStorageServiceMock

--- CurrencyWizardTests/LocalStorageServiceMock.swift
+++ CurrencyWizardTests/LocalStorageServiceMock.swift
@@ -10,24 +10,30 @@ import Foundation
 
 class LocalStorageServiceMock: LocalStorageService {
 	private var idsList: [String]
+	private let didCallListener: ((String) -> Void)?
 	
-	init(idsList: [String] = []) {
+	init(idsList: [String], didCallListener: ((String) -> Void)? = nil) {
 		self.idsList = idsList
+		self.didCallListener = didCallListener
 	}
 	
 	func requestLastUsedCurrencyOptions(completion: ((from: CurrencyOption, to: CurrencyOption)?) -> Void) {
+		self.didCallListener?("requestLastUsedCurrencyOptions")
 		completion(nil)
 	}
 	
 	func requestFavoriteCurrencyOptionIds(completion: ([String]) -> Void) {
+		self.didCallListener?("requestFavoriteCurrencyOptionIds")
 		completion(self.idsList)
 	}
 	
 	func favorite(currencyOption: CurrencyOption) {
+		self.didCallListener?("favorite(currencyOption:)")
 		self.idsList.append(currencyOption.id)
 	}
 	
 	func removeFavorite(currencyOption: CurrencyOption) {
+		self.didCallListener?("removeFavorite(currencyOption:)")
 		self.idsList = self.idsList.filter { $0 != currencyOption.id }
 	}
 }
