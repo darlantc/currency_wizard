a15322745aa8b649ff3c23877c3bbf1ad962f8c4
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-01-17 01:17:58 -0300

    feat: proved that LocalStorageServiceStub implements requestLastUsedCurrencyOptions

--- CurrencyWizardTests/LocalStorageServiceTests.swift
+++ CurrencyWizardTests/LocalStorageServiceTests.swift
@@ -15,13 +15,47 @@ class LocalStorageServiceTests: XCTestCase {
 		XCTAssertEqual(sut.internalCalls, [])
 	}
 	
+	func test_requestLastUsedCurrencyOptions_shouldCallMethod() {
+		var requestCount = 0
+		
+		func requestCallback(lastUsedOptions: (from: CurrencyOption, to: CurrencyOption)?) -> Void {
+			requestCount += 1
+		}
+		
+		let sut = makeSUT()
+		sut.requestLastUsedCurrencyOptions(completion: requestCallback)
+		XCTAssertEqual(requestCount, 1)
+		XCTAssertEqual(sut.internalCalls, ["requestLastUsedCurrencyOptions"])
+		
+		sut.requestLastUsedCurrencyOptions(completion: requestCallback)
+		XCTAssertEqual(requestCount, 2)
+		XCTAssertEqual(sut.internalCalls, ["requestLastUsedCurrencyOptions", "requestLastUsedCurrencyOptions"])
+	}
+	
+	func test_requestLastUsedCurrencyOptions_shouldReturnValidData() {
+		var response: (from: CurrencyOption, to: CurrencyOption)? = nil
+		
+		let sut = makeSUT()
+		sut.requestLastUsedCurrencyOptions { response = $0 }
+		
+		XCTAssertEqual(response?.from.id, "USD")
+		XCTAssertEqual(response?.to.id, "EUR")
+	}
+	
 	// MARK: Helpers
 	private func makeSUT() -> LocalStorageServiceStub {
 		return LocalStorageServiceStub()
 	}
 }
 
+private let usdCurrencyOption = CurrencyOption(name: "United States Dollar", id: "USD")
+private let eurCurrencyOption = CurrencyOption(name: "Euro", id: "EUR")
+
 private class LocalStorageServiceStub: LocalStorageService {
+	func requestLastUsedCurrencyOptions(completion: ((from: CurrencyOption, to: CurrencyOption)?) -> Void) {
+		completion((usdCurrencyOption, eurCurrencyOption))
+		self.didCall(function: "requestLastUsedCurrencyOptions")
+	}
 
 	var internalCalls = [String]()
 	func didCall(function name: String) {
