015ddc4623ea2b80b6a6cf2e03cacaa44c14e587
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-01-17 22:22:59 -0300

    feat: proved that SelectCurrencyOptionViewModel allows user to select a CurrencyOption and call didFinish callback with that selection

--- CurrencyWizard/SelectCurrencyOptionViewModel.swift
+++ CurrencyWizard/SelectCurrencyOptionViewModel.swift
@@ -8,11 +8,18 @@
 import Foundation
 
 final class SelectCurrencyOptionViewModel {
-	var currencyOptionsList = [CurrencyOption]()
+	private let didFinish: (CurrencyOption) -> Void
 	private let requestCurrencyOptionsUseCase: RequestCurrencyOptionsUseCase
+		
+	private var selectedIndex: Int? = nil
+	var currencyOptionsList = [CurrencyOption]()
 	
-	init(requestCurrencyOptionsUseCase: RequestCurrencyOptionsUseCase) {
+	init(
+		requestCurrencyOptionsUseCase: RequestCurrencyOptionsUseCase,
+		didFinish: @escaping (CurrencyOption) -> Void
+	) {
 		self.requestCurrencyOptionsUseCase = requestCurrencyOptionsUseCase
+		self.didFinish = didFinish
 	}
 	
 	func requestCurrencyOptions() {
@@ -21,5 +28,19 @@ final class SelectCurrencyOptionViewModel {
 			
 			self.currencyOptionsList = currencyOptions
 		}
+	}
+	
+	func didSelect(at index: Int) {
+		guard self.currencyOptionsList.count > index else {
+			self.selectedIndex = nil
+			return
+		}
+		self.selectedIndex = index
+	}
+	
+	func didFinishWithSelected() {
+		guard let index = self.selectedIndex, self.currencyOptionsList.count > index else { return }
+		
+		self.didFinish(self.currencyOptionsList[index])
 	}
 }
--- CurrencyWizardTests/SelectCurrencyOptionViewModelTests.swift
+++ CurrencyWizardTests/SelectCurrencyOptionViewModelTests.swift
@@ -29,13 +29,56 @@ class SelectCurrencyOptionViewModelTests: XCTestCase {
 		XCTAssertEqual(sut.currencyOptionsList.last!, eurCurrencyOption)
 	}
 	
+	func test_withoutOptionsFromCurrencyService_withDidFinishCallback_shouldNotCall() {
+		var count = 0
+		
+		let sut = makeSUT(options: [], didFinish: { _ in count += 1 })
+		sut.requestCurrencyOptions()
+		sut.didSelect(at: 0)
+		sut.didFinishWithSelected()
+		
+		XCTAssertEqual(count, 0)
+	}
+	
+	func test_withOptionsFromCurrencyServiceAndDidFinishCallback_didSelectAndDidFinish_shouldCallWhenNecessary() {
+		var count = 0
+		var selectedCurrencyOption: CurrencyOption? = nil
+		
+		let sut = makeSUT(options: [usdCurrencyOption, eurCurrencyOption], didFinish: { currencyOption in
+			count += 1
+			selectedCurrencyOption = currencyOption
+		})
+		sut.requestCurrencyOptions()
+		sut.didSelect(at: 0)
+		sut.didFinishWithSelected()
+		XCTAssertEqual(count, 1)
+		XCTAssertEqual(selectedCurrencyOption, usdCurrencyOption)
+		
+		sut.didSelect(at: 1)
+		sut.didFinishWithSelected()
+		XCTAssertEqual(count, 2)
+		XCTAssertEqual(selectedCurrencyOption, eurCurrencyOption)
+		
+		// Invalid index, should be ignored and assert equal last selected
+		sut.didSelect(at: 2)
+		sut.didFinishWithSelected()
+		XCTAssertEqual(count, 2)
+		XCTAssertEqual(selectedCurrencyOption, eurCurrencyOption)
+	}
+	
 	// MARK: Helpers
 	private let usdCurrencyOption = CurrencyOption(name: "United States Dollar", id: "USD")
 	private let eurCurrencyOption = CurrencyOption(name: "Euro", id: "EUR")
 
-	func makeSUT(options: [CurrencyOption] = []) -> SelectCurrencyOptionViewModel {
+	func makeSUT(
+		options: [CurrencyOption] = [],
+		didFinish: @escaping (CurrencyOption) -> Void = { _ in }
+	) -> SelectCurrencyOptionViewModel {
 		let currencyService = CurrencyServiceMock(options: options)
 		let requestCurrencyOptionsUseCase = RequestCurrencyOptionsUseCase(currencyService: currencyService)
-		return SelectCurrencyOptionViewModel(requestCurrencyOptionsUseCase: requestCurrencyOptionsUseCase)
+		return SelectCurrencyOptionViewModel(
+			requestCurrencyOptionsUseCase: requestCurrencyOptionsUseCase,
+			didFinish: didFinish
+		)
 	}
 }
