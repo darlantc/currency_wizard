0c9e6d8065549bb7580cd1c2076384cea1015ae9
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-01-20 07:49:09 -0300

    feat: added CurrencyServiceManager, responsible for managing REST API calls and local storage cache

--- CurrencyWizard/AppDelegate.swift
+++ CurrencyWizard/AppDelegate.swift
@@ -7,85 +7,20 @@
 
 import UIKit
 
-private class LocalStorageServiceMock: LocalStorageService {
-	fileprivate var currencyOptionsList = [CurrencyOption]()
-	private var idsList: [String]
-	private let didCallListener: ((String) -> Void)?
-	private var lastUsedFromCurrencyOption: CurrencyOption?
-	private var lastUsedToCurrencyOption: CurrencyOption?
-	
-	init(
-		idsList: [String] = [],
-		didCallListener: ((String) -> Void)? = nil,
-		lastUsedFromCurrencyOption: CurrencyOption? = nil,
-		lastUsedToCurrencyOption: CurrencyOption? = nil
-	) {
-		self.idsList = idsList
-		self.didCallListener = didCallListener
-		self.lastUsedFromCurrencyOption = lastUsedFromCurrencyOption
-		self.lastUsedToCurrencyOption = lastUsedToCurrencyOption
-	}
-	
-	func requestLastUsedCurrencyOptions(completion: ((from: CurrencyOption, to: CurrencyOption)?) -> Void) {
-		self.didCallListener?("requestLastUsedCurrencyOptions")
-		
-		guard let from = self.lastUsedFromCurrencyOption, let to = self.lastUsedToCurrencyOption else {
-			completion(nil)
-			return
-		}
-		completion((from, to))
-	}
-	
-	func saveLastUsedCurrencyOptions(from fromCurrencyOption: CurrencyOption, to toCurrencyOption: CurrencyOption) {
-		self.didCallListener?("saveLastUsedCurrencyOptions")
-		
-		self.lastUsedFromCurrencyOption = fromCurrencyOption
-		self.lastUsedToCurrencyOption = toCurrencyOption
-	}
-	
-	func save(currencyOptionsList: [CurrencyOption]) {
-		self.currencyOptionsList = currencyOptionsList
-		self.didCallListener?("requestLastUsedCurrencyOptions")
-	}
-	
-	func save(exchangeRates: [ExchangeRateQuote]) {
-		self.didCallListener?("save(exchangeRates:)")
-	}
-	
-	func requestFavoriteCurrencyOptionIds(completion: ([String]) -> Void) {
-		self.didCallListener?("requestFavoriteCurrencyOptionIds")
-		completion(self.idsList)
-	}
-	
-	func favorite(currencyOption: CurrencyOption) {
-		self.didCallListener?("favorite(currencyOption:)")
-		self.idsList.append(currencyOption.id)
-	}
-	
-	func removeFavorite(currencyOption: CurrencyOption) {
-		self.didCallListener?("removeFavorite(currencyOption:)")
-		self.idsList = self.idsList.filter { $0 != currencyOption.id }
-	}
-}
-
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		let window = UIWindow(frame: UIScreen.main.bounds)
-
-		
-		let httpService: HttpService = URLSessionHttpService()
-		let currencyLayerAPIService = CurrencyLayerAPIService(httpService: httpService) { _ in }
-		
 		let navigationController = UINavigationController()
 		
-		let currencyService: CurrencyService = currencyLayerAPIService
-		let localStorageService: LocalStorageService = LocalStorageServiceMock(
-			idsList: [],
-			lastUsedFromCurrencyOption: CurrencyOption(name: "United States Dollar", id: "USD"),
-			lastUsedToCurrencyOption: CurrencyOption(name: "Euro", id: "EUR")
+		let httpService: HttpService = URLSessionHttpService()
+		let currencyAPIService: CurrencyService = CurrencyLayerAPIService(httpService: httpService)
+		let localStorageService: LocalStorageService = UserDefaultsStorageService()
+		let currencyService: CurrencyService = CurrencyServiceManager(
+			currencyService: currencyAPIService,
+			localStorageService: localStorageService
 		)
 		
 		let factory = PresentationFactory(
@@ -93,7 +28,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 			currencyService: currencyService,
 			localStorageService: localStorageService
 		)
-		
 		factory.displayConvertCurrenciesViewController()
 		
 		window.rootViewController = navigationController
---
+++ CurrencyWizard/CurrencyServiceManager.swift
@@ -0,0 +1,82 @@
+//
+//  CurrencyServiceManager.swift
+//  CurrencyWizard
+//
+//  Created by Darlan Tódero ten Caten on 19/01/21.
+//
+
+import Foundation
+
+final class CurrencyServiceManager: CurrencyService {
+	let currencyService: CurrencyService
+	let localStorageService: LocalStorageService
+	
+	private var timer: Timer?
+	private var currencyOptions = [CurrencyOption]() {
+		didSet {
+			self.localStorageService.save(currencyOptionsList: currencyOptions)
+		}
+	}
+	private var exchangeRateQuotes = [ExchangeRateQuote]() {
+		didSet {
+			self.localStorageService.save(exchangeRates: exchangeRateQuotes)
+		}
+	}
+	
+	init(
+		currencyService: CurrencyService,
+		localStorageService: LocalStorageService
+	) {
+		self.currencyService = currencyService
+		self.localStorageService = localStorageService
+		
+		self.getDataFromCache(service: localStorageService)
+		self.refreshCache()
+		self.startRevalidatingTimer()
+	}
+	
+	func requestCurrencyOptions(completion: @escaping ([CurrencyOption]) -> Void) {
+		completion(self.currencyOptions)
+	}
+	
+	func requestExchangeRateQuotes(completion: @escaping ([ExchangeRateQuote]) -> Void) {
+		completion(self.exchangeRateQuotes)
+	}
+	
+	func requestExchangeRate(from: CurrencyOption, to: CurrencyOption, completion: @escaping (Double) -> Void) {
+		let exchangeRate = self.exchangeRateFrom(
+			from.id,
+			destination: to.id,
+			withQuotes: self.exchangeRateQuotes
+		)
+		completion(exchangeRate)
+	}
+	
+	private func getDataFromCache(service: LocalStorageService) {
+		service.requestCurrencyOptions { (currencyOptions) in
+			self.currencyOptions = currencyOptions
+		}
+		
+		service.requestExchangeRateQuotes { (exchangeRateQuotes) in
+			self.exchangeRateQuotes = exchangeRateQuotes
+		}
+	}
+	
+	private func startRevalidatingTimer() {
+		self.timer = Timer.scheduledTimer(timeInterval: 30, target: self, selector: #selector(refreshCache), userInfo: nil, repeats: true)
+	}
+	
+	@objc func refreshCache() {
+		self.currencyService.requestCurrencyOptions { (currencyOptions) in
+			self.currencyOptions = currencyOptions
+		}
+		
+		self.currencyService.requestExchangeRateQuotes { (exchangeRateQuotes) in
+			self.exchangeRateQuotes = exchangeRateQuotes
+		}
+	}
+	
+	deinit {
+		self.timer?.invalidate()
+	}
+}
